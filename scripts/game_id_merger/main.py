58d2cd73d2d736dc20508da0ef23c7af2ca3453b
Tabing010102 <tabing010102@outlook.com>
2024-01-16 15:27:21 +0800

    fix game_id_merger

--- scripts/game_id_merger/main.py
+++ scripts/game_id_merger/main.py
@@ -33,5 +33,5 @@ if __name__ == "__main__":
                 merged_manual_vndb_vid_list.append(vid)
         else:
             merged_data['entries'].append(entry)
-    with open('../../docs/schemas/game_id_connector_schema.json', 'w', encoding='utf8') as f3:
+    with open('../../docs/game_id_connector.json', 'w', encoding='utf8') as f3:
         f3.writelines(json.dumps(merged_data, ensure_ascii=False))
